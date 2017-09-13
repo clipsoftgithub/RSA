3949fb6ce26c12c4ae4a3990e731c251b1e84466
ByungChangYoo <ryu.byungchang@gmail.com>
2017-09-13 12:39:44 +0900

    - 진행에서 수정이 안되는 문제 해결

--- routes/process.js
+++ routes/process.js
@@ -12,30 +12,33 @@ router.get('/', function (req, res, next) {
     var id = req.param('task');
     var member = req.param('member');
     var record = {};
-    if(member != "" && member != undefined) {
+
+    function searchTask(id) {
+        db.get().collection('support_team').find({_id: new ObjectID(id)}).limit(1).toArray(function (err, record) {
+            if (err) {
+                console.error(err.message);
+            } else {
+                res.render('process', {task: record[0]});
+            }
+        });
+    };
+
+    if (member != "" && member != undefined) {
         record.member = member;
         record.state = "진행";
         record.btime = applyTimezoneOffset(new Date());
         record.etime = applyTimezoneOffset(new Date());
         db.get().collection('support_team').update({_id: new ObjectID(id)}, {$set: record}, {w: 1}, function (err) {
             if (err) {
-                console.warn(err.message);
+                console.error(err.message);
             }
             else {
-                console.log('successfully updated');
-                db.get().collection('support_team').find({_id: new ObjectID(id)}).limit(1).toArray(function (err, record) {
-                    if (err) {
-                        console.warn(err.message);
-                    } else {
-                        console.log(record);
-                        res.render('process', {task: record[0]});
-                    }
-                });
+                searchTask(id);
             }
         });
+    } else {
+        searchTask(id);
     }
-
-
 });
 
 module.exports = router;
